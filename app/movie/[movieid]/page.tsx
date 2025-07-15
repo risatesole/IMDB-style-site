90c03296914769c83366dfc16a9ba96648ac2d85
Henry Edilio Ramirez Chevalier <henry@edilio.dev>
2025-07-14 20:25:25 -0400

    migrate the movie details fetching from the movie details page to the api route and making small changes in movie details page

--- app/movie/[movieid]/page.tsx
+++ app/movie/[movieid]/page.tsx
@@ -1,26 +1,5 @@
 export const dynamic = "force-dynamic";
 
-function Requirements() {
-  return (
-    <>
-      <p>This is the movie details site</p>
-      <p>And these are the requirements:</p>
-      <ul>
-        <li>This page should have a big back cover with the movie image</li>
-        <li>
-          A small cover image (like a Facebook profile picture size) in front of
-          the big movie picture
-        </li>
-        <li>A brief description about the movie</li>
-        <li>A link to see the trailer</li>
-        <li>Photos of the movie like snippets of what is in the movie</li>
-        <li>List of the cast inside that movie</li>
-      </ul>
-    </>
-  );
-}
-
-
 import Link from "next/link";
 
 type PageProps = {
@@ -29,30 +8,23 @@ type PageProps = {
   };
 };
 
-export async function getMovieDetails(id: string) {
-  const apiKey = process.env.NEXT_THEMOVIEDB;
-  if (!apiKey) {
-    throw new Error("Missing NEXT_THEMOVIEDB API key in environment");
-  }
-
-  const url = `https://api.themoviedb.org/3/movie/${id}?api_key=${apiKey}&language=en-US`;
-  console.log("Fetching movie details:", url);
-
-  const res = await fetch(url, { cache: "no-store" });
-
-  console.log("TMDb API response status:", res.status);
+async function getMovieDetails(id: string) {
+  const res = await fetch(`/api/movie/details`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ movieid: id }),
+    cache: "no-store",
+  });
 
   if (!res.ok) {
     const errorText = await res.text();
-    console.error("Error response from TMDb:", errorText);
+    console.error("Error response from api:", errorText);
     throw new Error(`Failed to fetch movie details. Status: ${res.status}`);
   }
 
   const data = await res.json();
-  console.log("Movie details fetched successfully");
   return data;
 }
-
 
 export default async function MovieDetailsPage(props: PageProps) {
   const { params } = await props;
@@ -62,10 +34,8 @@ export default async function MovieDetailsPage(props: PageProps) {
 
   return (
     <div style={{ padding: "20px" }}>
-      <Requirements />
-
       <img
-        src={`https://image.tmdb.org/t/p/original${moviedata.belongs_to_collection.poster_path}`}
+        src={`https://image.tmdb.org/t/p/original${moviedata.belongs_to_collection?.poster_path}`}
         alt={`${moviedata.title} backdrop`}
         title="Front poster"
         style={{
@@ -87,10 +57,11 @@ export default async function MovieDetailsPage(props: PageProps) {
           marginTop: "20px",
         }}
       />
+
       <img
-        src={`https://image.tmdb.org/t/p/original${moviedata.belongs_to_collection.backdrop_path}`}
+        src={`https://image.tmdb.org/t/p/original${moviedata.belongs_to_collection?.backdrop_path}`}
         alt={`${moviedata.title} backdrop`}
-        title="backdroop of collection image"
+        title="Backdrop of collection image"
         style={{
           width: "100%",
           maxHeight: "400px",
@@ -112,24 +83,24 @@ export default async function MovieDetailsPage(props: PageProps) {
       </p>
 
       <p>
-        this movie belongs to collection:{" "}
-        <b>{moviedata.belongs_to_collection.name}</b>
+        This movie belongs to collection:{" "}
+        <b>{moviedata.belongs_to_collection?.name || "None"}</b>
       </p>
 
-      <p>Generos a los que pertenece: </p>
-      <p>{JSON.stringify(moviedata.genres, null, 2)}</p>
+      <p>Genres:</p>
+      <pre>{JSON.stringify(moviedata.genres, null, 2)}</pre>
 
       <p>
-        <b>Production company that made the movie</b>
+        <b>Production company:</b>
       </p>
       <p>{moviedata.production_companies[0]?.name}</p>
       <img
         src={`https://image.tmdb.org/t/p/w200${moviedata.production_companies[0]?.logo_path}`}
-        alt={`${moviedata.title} poster`}
+        alt={`${moviedata.title} production company logo`}
         style={{ borderRadius: "8px", marginTop: "20px" }}
       />
 
-      <p>Home page de la movie</p>
+      <p>Movie homepage:</p>
       <p>
         <Link href={moviedata.homepage}>Home page of the movie</Link>
       </p>
