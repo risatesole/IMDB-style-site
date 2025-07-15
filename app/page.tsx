4322476c0243c7a876e9f75898c0cd371e9722b4
Henry Edilio Ramirez Chevalier <henry@edilio.dev>
2025-07-14 20:42:01 -0400

    feat: add link to movie details page in Home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
     <div>
       <p>Welcome</p>
+      <p>go to <Link href={'/movie/299534'}>this page</Link> to see movie details based in id in browser url</p>
     </div>
   );
 }
