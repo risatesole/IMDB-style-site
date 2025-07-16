cdbf2db62ad4dd18e1a2e02e101a12539984e66c
Henry Edilio Ramirez Chevalier <henry@edilio.dev>
2025-07-15 23:31:46 -0400

    feat: add dynamic [showid] route with loading UI and show details page
    - Created `/show/[showid]/page.tsx` to fetch and display individual TV show details from TMDb
    - Added `getShowDetails()` helper with POST request to internal API
    - Rendered backdrop and poster images with controlled size and cropping
    - Included basic layout with title, overview, and homepage link
    - Added `/show/[showid]/loading.tsx` for friendly loading feedback

---
+++ app/show/[showid]/loading.tsx
@@ -0,0 +1,7 @@
+export default function Loading() {
+  return (
+    <div style={{ textAlign: "center", padding: "50px" }}>
+      <p>Hold tight the page is loading</p>
+    </div>
+  );
+}
---
+++ app/show/[showid]/page.tsx
@@ -0,0 +1,75 @@
+export const dynamic = "force-dynamic";
+import Link from "next/link";
+
+type PageProps = {
+  params: Promise<{ showid: string }>;
+};
+
+async function getShowDetails(id: string) {
+  const baseUrl = process.env.NEXT_PUBLIC_BASEAPIURL || "http://localhost:3000";
+  const res = await fetch(`${baseUrl}/api/show/details`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ showid: id }),
+    cache: "no-store",
+  });
+
+  if (!res.ok) {
+    const errorText = await res.text();
+    console.error("Error response from api:", errorText);
+    throw new Error(`Failed to fetch movie details. Status: ${res.status}`);
+  }
+
+  const data = await res.json();
+  return data;
+}
+
+function ShowJsonInFrontend(data: any) {
+  return (
+    <p>
+      <pre>{JSON.stringify(data, null, 2)}</pre>
+    </p>
+  );
+}
+
+export default async function MovieDetailsPage({ params }: PageProps) {
+  const { showid } = await params;
+
+  const showdata = await getShowDetails(showid);
+
+  return (
+    <div style={{ padding: "20px" }}>
+      <img
+        src={`https://image.tmdb.org/t/p/original${showdata.backdrop_path}`}
+        alt={`${showdata.title} backdrop`}
+        title="Front poster"
+        style={{
+          width: "100%",
+          height: "100%",
+          maxHeight: "400px",
+          objectFit: "cover",
+          marginTop: "20px",
+          objectPosition: "top",
+        }}
+      />
+      <img
+        src={`https://image.tmdb.org/t/p/original${showdata.poster_path}`}
+        alt={`poster`}
+        style={{
+          width: "auto",
+          height: "400px",
+          objectFit: "cover",
+          objectPosition: "top",
+        }}
+      />
+
+      <h2>{showdata.name}</h2>
+      <p>{showdata.overview}</p>
+      <p>
+        <Link href={showdata.homepage}>Show page</Link>
+      </p>
+
+      {/* <ShowJsonInFrontend data={showdata} /> */}
+    </div>
+  );
+}
