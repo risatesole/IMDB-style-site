f59d1b624695b4da2130bf6ad2ae762cea278e4c
Henry Edilio Ramirez Chevalier <henry@edilio.dev>
2025-07-16 08:00:35 -0400

    refactor(popular-page): replace hardcoded cards with dynamic card list
    - Removed CardRowComponent in favor of a reusable CardsPopularPage component
    - Introduced CardComponent and CardData type for better structure
    - Mapped API results to dynamically generate title, poster image, and redirect URL
    - Replaced individual card props with a cards array prop

--- app/popular/page.tsx
+++ app/popular/page.tsx
@@ -1,59 +1,52 @@
 import Link from "next/link";
 import styles from "./styles.module.css";
 
-// function CardComponent(
-//   title: string,
-//   details: string,
-//   posterimageurl: string,
-//   redirecturl: string
-// ) {
-//   return (
-//     <>
-//       <div>
-//         <p>Movie card</p>
-//         <Link href={`${redirecturl}`} passHref>
-//           <img
-//             src={`${redirecturl}`}
-//             alt={`${title} poster`}
-//             title={`${title} poster`}
-//             className={styles.posterImage}
-//           />
-//         </Link>
-
-//         <p>End of movie card</p>
-//       </div>
-//     </>
-//   );
-// }
-
-type CardRowProps = {
-  title: string;
-  // overview: string;
-  posterimageurl: string;
-  redirecturl: string;
-};
-
-function CardRowComponent({
-  title,
-  // overview,
-  posterimageurl,
-  redirecturl,
-}: CardRowProps) {
+function CardComponent(
+  title: string,
+  details: string,
+  posterimageurl: string,
+  redirecturl: string
+) {
   return (
     <>
       <div>
-        <Link href={redirecturl} passHref>
+        <p>Movie card</p>
+        <Link href={`${redirecturl}`} passHref>
           <img
-            src={posterimageurl}
+            src={`${redirecturl}`}
             alt={`${title} poster`}
             title={`${title} poster`}
             className={styles.posterImage}
           />
         </Link>
+
+        <p>End of movie card</p>
       </div>
     </>
   );
 }
+
+type CardData = {
+  title: string;
+  posterimageurl: string;
+  redirecturl: string;
+};
+
+function CardsPopularPage({ cards }: { cards: CardData[] }) {
+  return (
+    <div>
+      {cards.map((card, index) => (
+        <Link href={card.redirecturl} key={index}>
+          <div>
+            <img src={card.posterimageurl} alt={`${card.title} poster`} />
+            <p>{card.title}</p>
+          </div>
+        </Link>
+      ))}
+    </div>
+  );
+}
+
 
 async function getPopularTvshows() {
   const baseUrl = process.env.NEXT_PUBLIC_BASEAPIURL || "http://localhost:3000";
@@ -73,33 +66,26 @@ async function getPopularTvshows() {
   const data = await res.json();
   return data;
 }
-
 export default async function PopularPage() {
   const data = await getPopularTvshows();
+
+  const cardsData = data.results.map((item: any) => ({
+    title: item.name,
+    posterimageurl: `https://image.tmdb.org/t/p/w200${item.poster_path}`,
+    redirecturl: `/show/${item.id}`,
+  }));
+
   return (
     <div>
       <p>Welcome to the popular page</p>
-      <CardRowComponent
-        title={data.results[0].name}
-        // overview={data.results[0].overview}
-        posterimageurl={`https://image.tmdb.org/t/p/w200${data.results[0].poster_path}`}
-        redirecturl={`/show/${data.results[0].id}`}
-      />
+      <CardsPopularPage cards={cardsData} />
 
-      <CardRowComponent
-        title={data.results[1].name}
-        // overview={data.results[1].overview}
-        posterimageurl={`https://image.tmdb.org/t/p/w200${data.results[1].poster_path}`}
-        redirecturl={`/show/${data.results[1].id}`}
-      />
-
-      <p>id: {JSON.stringify(data.results[0].id, null, 2)}</p>
-      <p>name: {JSON.stringify(data.results[0].name, null, 2)}</p>
-      <p>poster:</p>
-
+      {/* 
       <p>
         <pre>{JSON.stringify(data, null, 2)}</pre>
-      </p>
+      </p> 
+      */}
     </div>
   );
 }
+
